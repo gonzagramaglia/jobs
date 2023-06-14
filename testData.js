9dca265cb5c9d456d1c76eb7d54f0319157cf557
Gonza <gonzagramaglia@gmail.com>
2023-06-14 13:51:19 -0300

    NearbyJobCard

--- testData.js
+++ testData.js
@@ -4,6 +4,7 @@ const testData = [
         job_title: 'Frontend Developer',
         Required: '6+ years as a React Developer',
         job_location: 'Bentonville',
+        employment_type: 'Full-time',
         employer_name: 'Airbnb',
         employer_logo: ''
     },
@@ -12,6 +13,7 @@ const testData = [
         job_title: 'React Native Developer',
         Required: '4+ years as a React Developer',
         job_location: 'New York',
+        employment_type: 'Full-time',
         employer_name: 'Amazon',
         employer_logo: ''
     },
@@ -20,6 +22,7 @@ const testData = [
         job_title: 'Full-stack Developer',
         Required: '3+ years as a React Developer',
         job_location: 'Texas',
+        employment_type: 'Part-time',
         employer_name: 'Google',
         employer_logo: ''
     },
@@ -28,6 +31,7 @@ const testData = [
         job_title: 'Frontend Developer',
         Required: '2+ years as a React Developer',
         job_location: 'California',
+        employment_type: 'Contractor',
         employer_name: 'Meta',
         employer_logo: ''
     },
@@ -36,6 +40,7 @@ const testData = [
         job_title: 'React Developer',
         Required: '5+ years as a React Developer',
         job_location: 'Ohio',
+        employment_type: 'Part-time',
         employer_name: 'Apple',
         employer_logo: ''
     }
